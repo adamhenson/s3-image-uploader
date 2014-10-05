b6b6a1a941e751280a36887740ea801fecf46959
Adam Henson <adamhenson1979@gmail.com>
2014-10-05 19:50:18 -0400

    basic setup

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var WebSocketServer = require('ws').Server;
+var s3 = require('s3');
 
-var Uploader = function(options) {
+var Uploader = function(options){
+  if(typeof options.server === 'undefined') throw new Error('Uploader: server is not defined.');
   this.options = options;
+};
+
+Uploader.prototype.upload = function(){
+
 };
 
 module.exports = Uploader;
